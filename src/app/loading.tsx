85f235f010e60389b0bf86e6cf9f2e3c9f9dfc1b
SornchaiTheDev <sornchaithedev@gmail.com>
2024-07-27 03:40:35 +0700

    chore: use Lottie Wrapper component instead original one

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import Lottie from "react-lottie";
 import loadingAniamtionData from "~/assets/lotties/loading.json";
+import LottieComp from "~/components/commons/Lottie";
 
 function LoadingComponent() {
-  const options = {
-    loop: true,
-    autoplay: true,
-    animationData: loadingAniamtionData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
     <div className="flex justify-center items-center h-screen">
-      <Lottie options={options} width={100} height={100} />
+      <LottieComp
+        animationData={loadingAniamtionData}
+        width={100}
+        height={100}
+      />
     </div>
   );
 }
