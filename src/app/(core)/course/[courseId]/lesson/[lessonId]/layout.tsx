d13fdb21c26267e5790d4ebfb9ab683cb10423f6
SornchaiTheDev <sornchaithedev@gmail.com>
2024-07-26 23:43:52 +0700

    feat: add lesson status at topbar

--- src/app/(core)/course/[courseId]/lesson/[lessonId]/layout.tsx
+++ src/app/(core)/course/[courseId]/lesson/[lessonId]/layout.tsx
@@ -1,18 +1,25 @@
 import type { ReactNode } from "react";
 import PageControl from "../_components/PageControl";
+import { CircleCheck } from "lucide-react";
 
 interface Props {
   children: ReactNode;
 }
- 
+
 function LessonLayout({ children }: Props) {
   return (
     <div className="relative">
-      <div className="sticky p-4 top-0 border-b bg-white">
-        <h5 className="text-gray-11 text-sm">Lesson 1.1</h5>
-        <h2 className="text-2xl mt-1 font-medium">
-          Repetition: <code>while</code>
-        </h2>
+      <div className="sticky p-4 top-0 border-b bg-white flex gap-4 items-center shadow-sm">
+        <CircleCheck size="2rem" className="text-grass-11" />
+        <div className="space-y-1">
+          <h5 className="text-gray-11 text-sm">Lesson 1.1</h5>
+          <h2 className="text-2xl font-medium">
+            Repetition: <code>while</code>
+          </h2>
+          <h4 className="text-sm">
+            Status: <span className="text-grass-11 font-semibold">Passed</span>
+          </h4>
+        </div>
       </div>
       <div className="p-4 container mx-auto max-w-4xl lg:px-10 prose prose-code:before:content-none prose-code:after:content-none font-anuphan">
         {children}
