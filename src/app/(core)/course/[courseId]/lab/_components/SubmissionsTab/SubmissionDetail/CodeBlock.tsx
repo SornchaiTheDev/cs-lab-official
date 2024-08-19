11d8d7d0e6d1674aaf8545a238cd1256eaf545ab
SornchaiTheDev <sornchaithedev@gmail.com>
2024-08-19 11:30:27 +0700

    chore: implement copy and codemirror in submission detail

--- src/app/(core)/course/[courseId]/lab/_components/SubmissionsTab/SubmissionDetail/CodeBlock.tsx
+++ src/app/(core)/course/[courseId]/lab/_components/SubmissionsTab/SubmissionDetail/CodeBlock.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import CodeMirror from "@uiw/react-codemirror";
+import { useEffect, useState } from "react";
+import CodeMirror from "../../CodeMirror";
 import { python } from "@codemirror/lang-python";
 import { Button } from "~/components/ui/button";
-import { Copy } from "lucide-react";
+import { Check, Copy } from "lucide-react";
+import { copyToClipboard } from "~/lib/copyToClipboard";
+import { cn } from "~/lib/utils";
 
 const hardCode = `print("Hello World")
 
@@ -13,19 +15,50 @@ const hardCode = `print("Hello World")
 `;
 
 function CodeBlock() {
+  const [isCopied, setIsCopied] = useState(false);
+
+  useEffect(() => {
+    if (isCopied) {
+      const timer = setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [isCopied]);
+
+  const handleCopy = () => {
+    copyToClipboard(hardCode);
+    setIsCopied(true);
+  };
+
   return (
     <div className="mt-4 mb-2 space-y-2">
       <div className="flex justify-between items-center">
         <h6 className="text-sm font-semibold text-gray-11">Code | Python3</h6>
-        <Button variant="secondary" size="sm" className="text-gray-11 space-x-2">
-          <Copy size="0.75rem" />
-          <span className="text-xs font-medium">Copy</span>
+        <Button
+          onClick={handleCopy}
+          variant="secondary"
+          size="sm"
+          className="text-gray-11 space-x-2"
+        >
+          {isCopied ? (
+            <Check className="text-grass-10" size="0.95rem" />
+          ) : (
+            <Copy size="0.75rem" />
+          )}
+
+          <span
+            className={cn("text-xs font-medium", isCopied && "text-grass-10")}
+          >
+            {isCopied ? "Copied!" : "Copy"}
+          </span>
         </Button>
       </div>
       <div className="border rounded-lg h-64 overflow-y-auto">
         <CodeMirror
           readOnly
           extensions={[python()]}
+          lang="python3"
           value={hardCode}
           height="100%"
         />
