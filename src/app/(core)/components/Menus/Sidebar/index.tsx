8d680568e7c8643c9943363ef1e41b1a7dd5f38a
SornchaiTheDev <sornchaithedev@gmail.com>
2024-08-23 23:19:31 +0700

    chore: add CS Lab brand to sidebar

--- src/app/(core)/components/Menus/Sidebar/index.tsx
+++ src/app/(core)/components/Menus/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Atom } from "lucide-react";
 import Course from "./Course";
 import SidebarWrapper from "./Wrapper";
 import { myCourses } from "~/__mocks__/myCourses";
@@ -6,6 +7,10 @@ import { myCourses } from "~/__mocks__/myCourses";
 function Sidebar() {
   return (
     <SidebarWrapper>
+      <div className="flex items-center gap-2 mt-2 mb-4">
+        <Atom size="2rem" className="text-gray-12" />
+        <h5 className="text-gray-12">CS Lab</h5>
+      </div>
       <h6 className="text-gray-11 text-sm font-light py-2">My Courses</h6>
       <div className="flex flex-col gap-4 mt-2">
         {myCourses.map((course) => (
