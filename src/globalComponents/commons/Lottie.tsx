91d096d821c7d2dc9667142a65397cb9923a8b98
SornchaiTheDev <sornchaithedev@gmail.com>
2025-05-04 19:40:37 +0700

    chore: use dynamic to import Lottie component

--- src/globalComponents/commons/Lottie.tsx
+++ src/globalComponents/commons/Lottie.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import Lottie, { type Options } from "react-lottie";
+import dynamic from "next/dynamic";
+import { type Options } from "react-lottie";
+const Lottie = dynamic(() => import("react-lottie"), {
+  ssr: false,
+});
 
 interface Props {
   animationData: any;
   width?: string | number;
   height?: string | number;
 }
+
 function LottieComp({ animationData, width, height }: Props) {
   const options: Options = {
     loop: true,
