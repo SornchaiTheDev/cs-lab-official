7aff2095fbff48b921cd850bf278962e5a0eaad3
SornchaiTheDev <sornchaithedev@gmail.com>
2025-05-05 17:01:29 +0700

    chore: add loading on login with google

--- src/app/auth/sign-in/_components/GoogleSignIn.tsx
+++ src/app/auth/sign-in/_components/GoogleSignIn.tsx
@@ -18,7 +18,7 @@ function GoogleSignIn() {
       className="w-full mt-8 border border-gray-6 px-5 py-2.5 rounded-lg flex items-center justify-center gap-4  bg-gray-2 hover:bg-gray-4"
     >
       <Image src={GoogleIcon} width={28} height={28} alt="Google Icon" />
-      <Loading {...{ isLoading }}>
+      <Loading {...{ isLoading }} fallback={<p>Signing in...</p>}>
         <p>Sign in with Google</p>
       </Loading>
     </button>
