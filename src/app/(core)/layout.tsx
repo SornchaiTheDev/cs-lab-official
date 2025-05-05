b28ebb8b2033e5b037b84f443a78012a5388402d
SornchaiTheDev <sornchaithedev@gmail.com>
2025-05-05 16:48:56 +0700

    chore: update jotai version and use Jotai Provider to prevent security risk

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -1,13 +1,18 @@
 import React, { type ReactNode } from "react";
 import CoreLayout from "./components/Menus/CoreLayout";
 import Sidebar from "./components/Menus/Sidebar";
+import { Provider as JotaiProvider } from "jotai";
 
 interface Props {
   children: ReactNode;
 }
 
 async function Layout({ children }: Props) {
-  return <CoreLayout Sidebar={<Sidebar />}>{children}</CoreLayout>;
+  return (
+    <JotaiProvider>
+      <CoreLayout Sidebar={<Sidebar />}>{children}</CoreLayout>
+    </JotaiProvider>
+  );
 }
 
 export default Layout;
