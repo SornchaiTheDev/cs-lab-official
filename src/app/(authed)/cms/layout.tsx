2be38442b3a9cc89e3ffa3d31eea975b7182f73d
SornchaiTheDev <sornchaithedev@gmail.com>
2025-05-09 19:29:11 +0700

    feat(auth): set up cms layout middleware

--- src/app/(authed)/cms/layout.tsx
+++ src/app/(authed)/cms/layout.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import CoreLayout from "~/layouts/CoreLayout";
+import { protectedMiddleware } from "~/middlewares/protected";
 import type { ChildrenProps } from "~/types/children-props";
 
-function Layout({ children }: ChildrenProps) {
+async function Layout({ children }: ChildrenProps) {
+  await protectedMiddleware((user) =>
+    user.roles.some((role) => ["teacher", "admin"].includes(role)),
+  );
+
   return <CoreLayout>{children}</CoreLayout>;
 }
 
