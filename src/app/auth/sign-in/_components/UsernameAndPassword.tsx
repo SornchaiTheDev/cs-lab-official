9385fdbecd99817e592d8fbe0331d2e72f09799d
SornchaiTheDev <sornchaithedev@gmail.com>
2025-05-09 16:07:02 +0700

    feat(auth): improve ux on username and password sign in

--- src/app/auth/sign-in/_components/UsernameAndPassword.tsx
+++ src/app/auth/sign-in/_components/UsernameAndPassword.tsx
@@ -44,34 +44,36 @@ function UsernameAndPassword() {
 
   return (
     <div className="flex flex-col gap-2 w-full mt-4">
-      <div>
-        <Label htmlFor="username">Username</Label>
-        <Input
-          id="username"
-          name="username"
-          value={username}
-          className="bg-gray-1 focus-visible:ring-0"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <Label htmlFor="password">Password</Label>
-        <Input
-          id="password"
-          name="password"
-          type="password"
-          value={password}
-          className="bg-gray-1 focus-visible:ring-0"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <Button
-        disabled={isLoading || isEmpty}
-        onClick={handleSignIn}
-        className="bg-grass-10 hover:bg-grass-11 mt-4"
-      >
-        {isLoading ? <LoaderCircle className="animate-spin" /> : "Sign In"}
-      </Button>
+      <form onSubmit={handleSignIn}>
+        <div>
+          <Label htmlFor="username">Username</Label>
+          <Input
+            id="username"
+            name="username"
+            value={username}
+            className="bg-gray-1 focus-visible:ring-0"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            className="bg-gray-1 focus-visible:ring-0"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <Button
+          disabled={isLoading || isEmpty}
+          onClick={handleSignIn}
+          className="bg-grass-10 hover:bg-grass-11 mt-4 w-full"
+        >
+          {isLoading ? <LoaderCircle className="animate-spin" /> : "Sign In"}
+        </Button>
+      </form>
     </div>
   );
 }
