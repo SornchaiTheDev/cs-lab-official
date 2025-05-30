adb678ae1dafd781b89c22118eb11ff71a622657
SornchaiTheDev <sornchaithedev@gmail.com>
2025-05-30 16:31:16 +0700

    chore: add EmojiPicker component

---
+++ src/components/commons/EmojiPicker.tsx
@@ -0,0 +1,46 @@
+// https://github.com/ferrucc-io/emoji-picker/blob/master/demo/src/playground/DefaultPickerExample.tsx
+import { EmojiPicker as FerruccEmojiPicker } from "@ferrucc-io/emoji-picker";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { useState } from "react";
+
+interface DefaultPickerExampleProps {
+  onEmojiSelect: (emoji: string) => void;
+}
+
+function EmojiPicker({ onEmojiSelect }: DefaultPickerExampleProps) {
+  const [selectedEmoji, setSelectedEmoji] = useState<string | null>(null);
+
+  const handleOnEmojiSelect = (emoji: string) => {
+    setSelectedEmoji(emoji);
+    onEmojiSelect(emoji);
+  };
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <div className="rounded-lg bg-(--gray-2) border border-(--gray-6) w-10 h-10 flex justify-center items-center">
+          {selectedEmoji}
+        </div>
+      </PopoverTrigger>
+      <PopoverContent className="p-0" side="bottom">
+        <FerruccEmojiPicker
+          className="border-none shadow-none"
+          onEmojiSelect={handleOnEmojiSelect}
+          emojisPerRow={8}
+          emojiSize={30}
+        >
+          <FerruccEmojiPicker.Header className="mt-2">
+            <FerruccEmojiPicker.Input
+              autoFocus={true}
+              placeholder="Search emoji"
+            />
+          </FerruccEmojiPicker.Header>
+          <FerruccEmojiPicker.Group>
+            <FerruccEmojiPicker.List containerHeight={400} />
+          </FerruccEmojiPicker.Group>
+        </FerruccEmojiPicker>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default EmojiPicker;
