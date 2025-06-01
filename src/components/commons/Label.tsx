ffec21a4904fa35dca1598b37800ea3160d6c5de
SornchaiTheDev <sornchaithedev@gmail.com>
2025-06-02 01:28:31 +0700

    chore: create our Label component from shadcn

---
+++ src/components/commons/Label.tsx
@@ -0,0 +1,18 @@
+import { Label as ShadcnLabel } from "~/components/ui/label";
+import type * as LabelPrimitive from "@radix-ui/react-label";
+import { cn } from "~/lib/utils";
+
+interface Props extends React.ComponentProps<typeof LabelPrimitive.Root> {
+  isError?: boolean;
+}
+
+function Label({ className, isError, ...props }: Props) {
+  return (
+    <ShadcnLabel
+      {...props}
+      className={cn(className, isError && "text-(--red-9)")}
+    />
+  );
+}
+
+export default Label;
