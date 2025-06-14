a52545644abe5ca645e82e03c3672d303ec27764
SornchaiTheDev <sornchaithedev@gmail.com>
2025-06-15 01:43:05 +0700

    chore: update check isNoData state

--- src/app/(authed)/cms/courses/page.tsx
+++ src/app/(authed)/cms/courses/page.tsx
@@ -32,7 +32,10 @@ function CMSCoursePage() {
     sortOrder: "asc",
   });
 
-  const isNoData = coursePagination.pages.length === 0 && !isFetching;
+  const isNoData =
+    search.length > 0 &&
+    coursePagination.pages.every((page) => page.data.length === 0) &&
+    !isFetching;
 
   const bottomDivRef = useRef<HTMLDivElement>(null);
 
@@ -62,7 +65,7 @@ function CMSCoursePage() {
   }, [isFetching, fetchNextPage, hasNextPage]);
 
   return (
-    <div className="@container w-full max-w-[1920px] mx-auto h-screen flex flex-col">
+    <div className="@container w-full max-w-[1920px] mx-auto h-screen flex flex-col mb-20">
       <div className="flex justify-between w-full">
         <h3 className="text-3xl font-medium">Courses</h3>
         <div className="flex items-center gap-4">
