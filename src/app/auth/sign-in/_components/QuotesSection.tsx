5a16e796eac12c8eb0e432aef54f1151b2a44c98
SornchaiTheDev <sornchaithedev@gmail.com>
2025-07-13 14:11:17 +0700

    chore: use local quotes instead

--- src/app/auth/sign-in/_components/QuotesSection.tsx
+++ src/app/auth/sign-in/_components/QuotesSection.tsx
@@ -9,49 +9,29 @@ import BG5 from "~/assets/quotes_bg/bg5.jpeg";
 import BG6 from "~/assets/quotes_bg/bg6.jpeg";
 import BG7 from "~/assets/quotes_bg/bg7.jpeg";
 import BG8 from "~/assets/quotes_bg/bg8.jpeg";
-import axios from "axios";
+import quotes from "~/constants/quotes.json";
 
 interface QuoteResponse {
-  _id: string;
   // The quotation text
   content: string;
   // The full name of the author
   author: string;
-  // The `slug` of the quote author
-  authorSlug: string;
-  // The length of quote (number of characters)
-  length: number;
-  // An array of tag names for this quote
-  tags: string[];
 }
 
 function QuotesSection() {
-  const [quote, setQuote] = useState(
-    'Every developer starts their journey with "Hello World"',
-  );
-  const [author, setAuthor] = useState("SornchaiTheDev");
-  const [isLoading, setIsLoading] = useState(true);
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
   const [imageIdx, setImageIdx] = useState<number | null>(null);
   const [images, _] = useState([BG1, BG2, BG3, BG4, BG5, BG6, BG7, BG8]);
 
-  const isStateEmpty = quote === "" || author === "";
-  const isNoQuote = isLoading || isStateEmpty;
-
   useEffect(() => {
-    const getQuote = async () => {
-      try {
-        const res = await axios.get<QuoteResponse>(
-          "https://api.quotable.io/random?tags=technology&maxLength=120",
-        );
-        setQuote(res.data.content);
-        setAuthor(res.data.author);
-      } catch (err) {
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getQuote();
+    const randIdx = Math.max(
+      Math.floor(Math.random() * quotes.list.length) - 1,
+      0,
+    );
+    const quote = quotes.list[randIdx] as QuoteResponse;
+    setQuote(quote.content);
+    setAuthor(quote.author);
   }, []);
 
   useEffect(() => {
@@ -59,11 +39,13 @@ function QuotesSection() {
     setImageIdx(randIdx);
   }, [images]);
 
+  const isHasQuote = quote !== "" && author !== "";
+
   return (
     <div className="flex-1 max-h-[300px] md:max-h-[450px] lg:max-h-full relative rounded-3xl overflow-hidden">
       <div className="absolute top-0 bottom-0 left-0 right-0 backdrop-blur-xs bg-black/40 z-10">
-        {!isNoQuote && (
-          <div className="flex flex-col justify-end pb-10 items-center h-full gap-4">
+        {isHasQuote && (
+          <div className="flex flex-col justify-end pb-10 h-full gap-4">
             <div className="px-10">
               <h1 className="text-2xl lg:text-4xl font-medium text-white lg:leading-12">
                 {quote}
