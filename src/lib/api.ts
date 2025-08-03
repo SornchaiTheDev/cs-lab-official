e499975cd96e7ba3a08a02682c60d94b22ec31f9
SornchaiTheDev <sornchaithedev@gmail.com>
2025-08-03 12:43:40 +0700

    chore: add axios interceptor to refresh token

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,3 +5,19 @@ export const api = axios.create({
   baseURL: env("API_URL"),
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  function onFulfilled(response) {
+    return response;
+  },
+  async function onRejected(error) {
+    const originalRequest = error.config;
+    if (error.response?.status === 401) {
+      await fetch("/auth/refresh-token", {
+        credentials: "include",
+      });
+
+      return api(originalRequest);
+    }
+  },
+);
